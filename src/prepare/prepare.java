84860eca7c8846fea4d23bc25a9c25b28da158d8
seonghoon <shee5032@naver.com>
2022-10-25 15:05:51 +0900

    commit test라인 추가 -psh

--- src/prepare/prepare.java
+++ src/prepare/prepare.java
@@ -5,6 +5,7 @@ public class prepare {
 		System.out.println("�������Դϴ�.");
 		System.out.println("���� �����ε���");
 		System.out.println("다시 utf-8로 버전을 바꾸었습니당");
+		System.out.println("test - psh");
 	}
 
 }
