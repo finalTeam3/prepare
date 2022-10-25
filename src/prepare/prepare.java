3b159191f2969ab5545865a6f3481f3a279cad9b
yyboom <yoonyoung37@naver.com>
2022-10-25 17:15:10 +0900

    commit-chobranch

--- src/prepare/prepare.java
+++ src/prepare/prepare.java
@@ -17,6 +17,9 @@ public class prepare {
 		
 		System.out.println("혜정 브랜치 테스트입니다");
 		System.out.println("브랜치 마스터로 합치기~");
+		
+		System.out.println("혜정 브랜치 테스트입니다");
+		System.out.println("혜정 브랜치 테스트입니다");
 	}
 
 }
