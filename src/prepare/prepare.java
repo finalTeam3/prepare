1d5f6f36f86cf114b4e28ce354b295f9f26c7452
Shindongjoon7 <ehdwns8467@gmail.com>
2022-10-25 15:49:10 +0900

    test sdj

--- src/prepare/prepare.java
+++ src/prepare/prepare.java
@@ -2,10 +2,11 @@ package prepare;
 
 public class prepare {
 	private void sysout() {
-		System.out.println("�������Դϴ�.");
-		System.out.println("���� �����ε���");
-		System.out.println("다시 utf-8로 버전을 바꾸었습니당");
+		System.out.println("占쏙옙占쏙옙占쏙옙占쌉니댐옙.");
+		System.out.println("占쏙옙占쏙옙 占쏙옙占쏙옙占싸듸옙占쏙옙");
+		System.out.println("�떎�떆 utf-8濡� 踰꾩쟾�쓣 諛붽씀�뿀�뒿�땲�떦");
 		System.out.println("test - psh");
+		System.out.println("test - sdh");
 	}
 
 }
